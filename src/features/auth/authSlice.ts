57be09af768c85d8c9c6918c71b9f8d4a03845b5
paikpaik <woduf9311@gmail.com>
2023-08-17 01:16:37 +0900

    Add: auth register redux

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,63 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { DisplayUser } from "./models/DisplayUser.interface";
+import { Jwt } from "./models/jwt";
+import { NewUser } from "./models/NewUser";
+import authService from "./services/auth.service";
+
+// TODO: move higher
+interface AsyncState {
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+}
+
+interface AuthState extends AsyncState {
+  user?: DisplayUser | null;
+  jwt?: Jwt;
+  isAuthenticated?: boolean;
+}
+
+const initialState: AuthState = {
+  user: null, // user
+  jwt: null, // jwt
+  isAuthenticated: false,
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user: NewUser, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      return thunkAPI.rejectWithValue("Unable to register!");
+    }
+  }
+);
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // REGISTER
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.user = null;
+      });
+  },
+});
+
+export default authSlice.reducer;
