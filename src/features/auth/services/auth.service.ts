b40fd6c20d0845e04bbb190f8f16da90e94d730e
paikpaik <woduf9311@gmail.com>
2023-08-18 13:30:12 +0900

    Add: auth login Service 연결

--- src/features/auth/services/auth.service.ts
+++ src/features/auth/services/auth.service.ts
@@ -1,6 +1,10 @@
+import { DecodedJwt } from "./../models/DecodedJwt.interface";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 import { DisplayUser } from "../models/DisplayUser.interface";
 import { NewUser } from "../models/NewUser";
+import { LoginUser } from "../models/LoginUser.interface";
+import { Jwt } from "../models/jwt";
 
 const register = async (newUser: NewUser): Promise<DisplayUser | null> => {
   const response = await axios.post(
@@ -11,9 +15,25 @@ const register = async (newUser: NewUser): Promise<DisplayUser | null> => {
   return response.data;
 };
 
+const login = async (user: LoginUser): Promise<Jwt> => {
+  const response = await axios.post(
+    `${process.env.REACT_APP_BASE_API}/auth/login`,
+    user
+  );
+
+  if (response.data) {
+    localStorage.setItem("jwt", JSON.stringify(response.data));
+
+    const decodedJwt: DecodedJwt = jwt_decode(response.data.token);
+    localStorage.setItem("user", JSON.stringify(decodedJwt.user));
+  }
+
+  return response.data;
+};
+
 const authService = {
   register,
-  // login,
+  login,
   // logout,
   // verifyJwt,
 };
