3d6b201b401a1a9ffdd1c21e5a5f6739b15a7185
paikpaik <woduf9311@gmail.com>
2023-08-25 11:59:03 +0900

    Add: HomePage logo, username, logout, cart 코드 추가

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,36 +1,9 @@
-import { useEffect } from "react";
-import { logout, selectedUser } from "../features/auth/authSlice";
-import { useAppDispatch, useAppSelector } from "../hooks/redux/hooks";
+import HeaderComponent from "../features/products/components/Header.component";
 
 const HomePage = () => {
-  const dispatch = useAppDispatch();
-
-  const { user, jwt } = useAppSelector(selectedUser);
-
-  useEffect(() => {
-    console.log(123, user, jwt);
-  }, [user]);
-
-  const logoutHandler = () => {
-    dispatch(logout());
-  };
-
   return (
     <div>
-      <h1>HomePage</h1>
-      <a
-        onClick={logoutHandler}
-        style={{
-          backgroundColor: "yellow",
-          cursor: "pointer",
-          height: "40px",
-          width: "60px",
-          padding: "8px",
-        }}
-      >
-        Logout
-      </a>
-      {user?.email}
+      <HeaderComponent />
     </div>
   );
 };
