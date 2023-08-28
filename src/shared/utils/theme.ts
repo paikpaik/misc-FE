eb9a4a74e9c88792f9b24de19c2bee9b9f254584
paikpaik <woduf9311@gmail.com>
2023-08-28 18:37:25 +0900

    Fix: 메인컬러 변경

--- src/shared/utils/theme.ts
+++ src/shared/utils/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#ff9900",
+      main: "#ee1770",
       contrastText: "#000000",
     },
     secondary: {
